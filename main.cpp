b21ca01ae0be81d2e71c650940678ba06216bec7
wenkai-Yu <2019905327@chd.edu.cn>
2020-05-13 21:19:56 +0800

    主函数
    引入其它头文件，并开始程序。

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include "System_Interfances.h"
+#include "_StuIno_.h"
+//#include "_StuIno_.h"
+#include "Passwords_Management.h"
+using namespace std;
+int main()
+{
+	system("color 70");
+	system("mode con cols=115 lines=30");
+	StarInt();
+	return 0;
+}
